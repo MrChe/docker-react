8f2e7b6f4c908f1054783e6ba74c77b4c9558900
Leonid Kuznetsov <gitmrche@gmail.com>
2020-06-28 15:51:44 +0300

    changed App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I was change from feature/app branch
         </a>
       </header>
     </div>
