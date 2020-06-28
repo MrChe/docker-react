94e49284af4273c894c949ae50fc82a4193c81b6
Leonid Kuznetsov <gitmrche@gmail.com>
2020-06-28 15:56:05 +0300

    update test App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was change from feature/app branch
+          I was change from feature branch
         </a>
       </header>
     </div>
